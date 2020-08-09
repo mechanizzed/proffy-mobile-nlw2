98bfea7f678a38dac09fda2b25cc37c732508522
mechanizzed <dsibr@hotmail.com>
2020-08-09 19:30:09 -0300

    Add: GiveClasses page completed

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -2,14 +2,36 @@ import React from 'react';
 import { View, Text, Image, Button } from 'react-native';
 
 // images and icons
+import GiveClassesBackgroundImage from '../../assets/images/give-classes-background.png';
 
 // styles
-import { Container } from './styles';
+import {
+  Container,
+  BackgroundImageContainer,
+  Title,
+  Description,
+  ActionButton,
+  ButtonText,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const GiveClasses = () => {
+  const navigation = useNavigation();
   return (
     <Container>
-      <Text>GiveClasses</Text>
+      <BackgroundImageContainer
+        resizeMode="contain"
+        source={GiveClassesBackgroundImage}
+      >
+        <Title>Quer ser um Proffy?</Title>
+        <Description>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Description>
+      </BackgroundImageContainer>
+      <ActionButton onPress={() => navigation.goBack()}>
+        <ButtonText>Tudo bem</ButtonText>
+      </ActionButton>
     </Container>
   );
 };
--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -1,8 +1,47 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   background-color: #8257e5;
   justify-content: center;
   padding: 40px;
+`;
+
+export const BackgroundImageContainer = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Archivo_700Bold';
+  color: #fff;
+  font-size: 32px;
+  line-height: 37px;
+  max-width: 180px;
+`;
+
+export const Description = styled.Text`
+  margin-top: 24px;
+  color: #d4c2ff;
+  font-size: 16px;
+  line-height: 18px;
+  font-family: 'Poppins_400Regular';
+  max-width: 240px;
+`;
+
+export const ActionButton = styled(RectButton)`
+  align-self: stretch;
+  background-color: #04d361;
+  border-radius: 8px;
+  margin: 40px 0;
+  justify-content: center;
+  align-items: center;
+  height: 58px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Archivo_700Bold';
+  font-size: 16px;
+  color: #fff;
 `;
